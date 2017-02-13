46d33c87d24ef9bd4b5ef01f06e26eaca4da51c4
Katie Salter <katie.j.salter@gmail.com>
2017-02-13 11:52:49 +0000

    happy

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,22 @@
+class CoachingController < ApplicationController
+  def answer
+    @query = params[:query]
+    @response = coach_answer(@query)
+  end
+
+  def ask
+
+  end
+
+  def coach_answer(your_message)
+    # TODO: return coach answer to your_message
+    if your_message == "I am going to work right now!"
+      return ""
+    elsif your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
